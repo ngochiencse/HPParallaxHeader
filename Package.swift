28f4f4e4758a00f1954cd5ff23fceb333d7319ee
ngochiencse <ngochiencse@gmail.com>
2021-06-06 10:49:12 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .target(
             name: "HPParallaxHeader",
             dependencies: [],
-            path: "HPParallaxHeader",
+            path: "HPParallaxHeader/Classes",
             publicHeadersPath: ".")
     ]
 )
